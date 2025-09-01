badebb10be2c5c8717ce53a175d71f2052e1e6dc
Verissimacollection <verissima.collection@gmail.com>
2025-09-01 18:41:43 +0000

    testes

--- src/data/conjunto_mirela.ts
+++ src/data/conjunto_mirela.ts
@@ -14,86 +14,19 @@ export const conjunto_mirela: Product[] = [
     description: "Conjunto Mirela disponível em múltiplos tamanhos",
     sku: "2217",
     inStock: true,
-    color: "Areia",
+    colors: [
+      { color: "Beje", inStock: true },
+      { color: "Azul Bebe", inStock: true },
+      { color: "Branco", inStock: true },
+      { color: "Marinho", inStock: true },
+      { color: "Verde", inStock: true },
+      
+    ],
     sizes: [
       { size: "P", inStock: true },
       { size: "M", inStock: true },
       { size: "G", inStock: true }
     ]
   },
-   {
-      id: 7,
-      name: "Conjunto Mirela",  // Nome sem tamanho
-      price: 60.00,
-      images: [
-        "/images/conjuntos/conjunto_mirela/branco.jpeg",
-        "/images/conjuntos/conjunto_mirela/cores.jpeg",
-      ],
-      category: "Conjuntos",
-      description: "Conjunto Mirela disponível em múltiplos tamanhos",
-      sku: "2217",
-      inStock: true,
-      color: "Branco",
-      sizes: [
-        { size: "P", inStock: true },
-        { size: "M", inStock: true },
-        { size: "G", inStock: true }
-      ]
-    },
- {
-    id: 8,
-    name: "Conjunto Mirela",  // Nome sem tamanho
-    price: 60.00,
-    images: [
-      "/images/conjuntos/conjunto_mirela/azulbebe.jpeg",
-      "/images/conjuntos/conjunto_mirela/cores.jpeg",
-    ],
-    category: "Conjuntos",
-    description: "Conjunto Mirela disponível em múltiplos tamanhos",
-    sku: "2217",
-    inStock: true,
-    color: "Azul Bebê",
-    sizes: [
-      { size: "P", inStock: true },
-      { size: "M", inStock: true },
-      { size: "G", inStock: true }
-    ]
-  }, {
-        id: 9,
-        name: "Conjunto Mirela",  // Nome sem tamanho
-        price: 60.00,
-        images: [
-          "/images/conjuntos/conjunto_mirela/marinho.jpeg",
-          "/images/conjuntos/conjunto_mirela/cores.jpeg",
-        ],
-        category: "Conjuntos",
-        description: "Conjunto Mirela disponível em múltiplos tamanhos",
-        sku: "2217",
-        inStock: true,
-        color: "Marinho",
-        sizes: [
-          { size: "P", inStock: true },
-          { size: "M", inStock: true },
-          { size: "G", inStock: true }
-        ]
-      },
-   {
-      id: 10,
-      name: "Conjunto Mirela",  // Nome sem tamanho
-      price: 60.00,
-      images: [
-        "/images/conjuntos/conjunto_mirela/verde.jpeg",
-        "/images/conjuntos/conjunto_mirela/cores.jpeg",
-      ],
-      category: "Conjuntos",
-      description: "Conjunto Mirela disponível em múltiplos tamanhos",
-      sku: "2217",
-      inStock: true,
-      color: "Verde",
-      sizes: [
-        { size: "P", inStock: true },
-        { size: "M", inStock: true },
-        { size: "G", inStock: true }
-      ]
-    },
+   
 ];
