672464eb83ace4b2641126531841fd6f954e28fb
Verissimacollection <verissima.collection@gmail.com>
2025-09-01 18:58:08 +0000

    testes

---
+++ src/data/conjunto_juliana.ts
@@ -0,0 +1,32 @@
+import { Product } from "@/interfaces/Product";
+
+// Exemplo de como estruturar produtos com múltiplos tamanhos
+export const juliana: Product[] = [
+  {
+    id: 1,
+    name: "Conjunto Juliana",  // Nome sem tamanho
+    price: 60.00,
+    images: [
+      "/images/conjuntos/juliana/branco.jpeg",
+      "/images/conjuntos/juliana/cores.jpeg",
+    ],
+    category: "Conjuntos",
+    description: "Conjunto Bia disponível em múltiplos tamanhos",
+    sku: "1463",
+    inStock: true,
+    colors: [
+      { color: "Branco", inStock: true },
+      { color: "Azul Bebe", inStock: true },
+      { color: "Beje", inStock: true },
+      { color: "Preto", inStock: true },
+      
+    ],
+    sizes: [
+      { size: "P", inStock: true },
+      { size: "M", inStock: true },
+      { size: "G", inStock: true }
+    ]
+  },
+
+  
+];
