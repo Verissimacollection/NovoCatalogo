c675d51ebab73fb3baba047b00ff9eafedd2bd1f
Verissimacollection <verissima.collection@gmail.com>
2025-09-01 18:39:05 +0000

    testes

--- src/data/conjunto_bia.ts
+++ src/data/conjunto_bia.ts
@@ -14,81 +14,19 @@ export const conjunto_bia: Product[] = [
     description: "Conjunto Bia disponível em múltiplos tamanhos",
     sku: "6532",
     inStock: true,
-    color: "Rosa Bebê",
+    colors: [
+      { color: "Verde", inStock: true },
+      { color: "Azul Bebe", inStock: true },
+      { color: "Amarelo", inStock: true },
+      { color: "Preto", inStock: true },
+      { color: "Rosa Bebe", inStock: true }
+    ],
     sizes: [
       { size: "P", inStock: true },
       { size: "M", inStock: true },
       { size: "G", inStock: true }
     ]
   },
-  {
-    id: 2,
-    name: "Conjunto Bia",  // Nome sem tamanho
-    price: 60.00,
-    images: [
-      "/images/conjuntos/conjunto_bia/biacores.jpeg",
-    ],
-    category: "Conjuntos",
-    description: "Conjunto Bia disponível em múltiplos tamanhos",
-    sku: "6532",
-    inStock: true,
-    color: "Verde",
-    sizes: [
-      { size: "P", inStock: true },
-      { size: "M", inStock: true },
-      { size: "G", inStock: true }
-    ]
-  },{
-    id: 3,
-    name: "Conjunto Bia",  // Nome sem tamanho
-    price: 60.00,
-    images: [
-      "/images/conjuntos/conjunto_bia/biacores.jpeg",
-    ],
-    category: "Conjuntos",
-    description: "Conjunto Bia disponível em múltiplos tamanhos",
-    sku: "6532",
-    inStock: true,
-    color: "Azul Bebê",
-    sizes: [
-      { size: "P", inStock: true },
-      { size: "M", inStock: true },
-      { size: "G", inStock: true }
-    ]
-  },{
-    id: 4,
-    name: "Conjunto Bia",  // Nome sem tamanho
-    price: 60.00,
-    images: [
-      "/images/conjuntos/conjunto_bia/biacores.jpeg",
-    ],
-    category: "Conjuntos",
-    description: "Conjunto Bia disponível em múltiplos tamanhos",
-    sku: "6532",
-    inStock: true,
-    color: "Amarelo",
-    sizes: [
-      { size: "P", inStock: true },
-      { size: "M", inStock: true },
-      { size: "G", inStock: true }
-    ]
-  },
-  {
-    id: 5,
-    name: "Conjunto Bia",  // Nome sem tamanho
-    price: 60.00,
-    images: [
-      "/images/conjuntos/conjunto_bia/biacores.jpeg",
-    ],
-    category: "Conjuntos",
-    description: "Conjunto Bia disponível em múltiplos tamanhos",
-    sku: "6532",
-    inStock: true,
-    color: "Preto",
-    sizes: [
-      { size: "P", inStock: true },
-      { size: "M", inStock: true },
-      { size: "G", inStock: true }
-    ]
-  },
+
+  
 ];
