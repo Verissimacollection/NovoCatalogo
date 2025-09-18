67054f0570e79ba152e8284fe80ebe45860ad1e9
Verissimacollection <verissima.collection@gmail.com>
2025-09-18 21:00:39 +0000

    testes

---
+++ src/data/conjunto_valdirene.ts
@@ -0,0 +1,31 @@
+import { Product } from "@/interfaces/Product";
+
+// Exemplo de como estruturar produtos com múltiplos tamanhos
+export const conjunto_valdirene: Product[] = [
+  {
+    id: 60,
+    name: "Conjunto Valdirene ",  // Nome sem tamanho
+    price: 190.00,
+    images: [
+      
+      "/images/conjuntos/conjunto_valdirene/cores.jpeg",
+    ],
+    category: "Conjuntos",
+    description: "Conjunto Valdirene 210,00 Varejo",
+    sku: "60",
+    inStock: true,
+    colors: [
+      { color: "Rosa", inStock: true },
+      { color: "Azul Bebe", inStock: true },
+      { color: "Verde", inStock: true },
+      { color: "Amarelo", inStock: true },
+      
+    ],
+    sizes: [
+      { size: "P", inStock: true },
+      { size: "M", inStock: true },
+      { size: "G", inStock: true }
+    ]
+  },
+  
+];
