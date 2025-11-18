464c8b0a8c421b1f4a661b35cee6fc51ff5c6390
Verissimacollection <verissima.collection@gmail.com>
2025-11-18 17:18:11 +0000

    testes

--- src/data/vestido_tubinho.ts
+++ src/data/vestido_tubinho.ts
@@ -7,9 +7,9 @@ export const vestido_tubinho: Product[] = [
         name: "Vestido Tubinho",  // Nome sem tamanho
         price: 190.00,
         images: [
-            "/images/vestidos/vestido tubinho/branco.webp",
-             "/images/vestidos/vestido tubinho/preto.webp",
-              "/images/vestidos/vestido tubinho/cores.webp",
+            "/images/vestidos/Vestido tubinho/branco.webp",
+             "/images/vestidos/Vestido tubinho/preto.webp",
+              "/images/vestidos/Vestido tubinho/cores.webp",
             
           
         ],
