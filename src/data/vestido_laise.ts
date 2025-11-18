845f12b9a94965d60ab486330aff0c888a0d3f74
Verissimacollection <verissima.collection@gmail.com>
2025-11-18 17:24:12 +0000

    laise

---
+++ src/data/vestido_laise.ts
@@ -0,0 +1,34 @@
+import { Product } from "@/interfaces/Product";
+
+// Exemplo de como estruturar produtos com múltiplos tamanhos
+export const vestido_laise: Product[] = [
+    {
+        id: 356,
+        name: "Vestido Laise",  // Nome sem tamanho
+        price: 190.00,
+        images: [
+            "/images/vestidos/vestido_Laise/amarelo.webp",
+             "/images/vestidos/vestido_Laise/cores.webp",
+          
+        ],
+        category: "Vestido",
+        description: "Vestido Laise Varejo R$220,00  ",
+        sku: "304",
+        inStock: true,
+        colors: [
+            { color: "Branco", inStock: true },
+            { color: "Amarelo", inStock: true },
+            { color: "Verde", inStock: true },
+              { color: "Azul", inStock: true },
+                { color: "Rosa", inStock: true },
+            
+        ],
+        sizes: [
+            { size: "P", inStock: true },
+            { size: "M", inStock: true },
+            { size: "G", inStock: true },
+            { size: "GG", inStock: true }
+        ]
+    },
+
+];
