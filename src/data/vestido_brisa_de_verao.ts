eb82f51d4adeb563842c7df3dae6b4ac64d174a2
Verissimacollection <verissima.collection@gmail.com>
2025-11-18 17:33:07 +0000

    testes

---
+++ src/data/vestido_brisa_de_verao.ts
@@ -0,0 +1,34 @@
+import { Product } from "@/interfaces/Product";
+
+// Exemplo de como estruturar produtos com múltiplos tamanhos
+export const vestido_Brisa_de_Verão: Product[] = [
+    {
+        id: 356,
+        name: "Vestido Brisa de Verão",  // Nome sem tamanho
+        price: 190.00,
+        images: [
+            "/images/vestidos/Vestido Brisa de Verão/branco.jpg",
+             "/images/vestidos/Vestido Brisa de Verão/cores.jpg",
+          
+        ],
+        category: "Vestido",
+        description: "Vestido Vestido Brisa de Verão Varejo R$220,00  ",
+        sku: "304",
+        inStock: true,
+        colors: [
+            { color: "Branco", inStock: true },
+            { color: "Amarelo", inStock: true },
+            { color: "Verde", inStock: true },
+              { color: "Azul", inStock: true },
+               
+            
+        ],
+        sizes: [
+            { size: "P", inStock: true },
+            { size: "M", inStock: true },
+            { size: "G", inStock: true },
+            { size: "GG", inStock: true }
+        ]
+    },
+
+];
